d100a69161822eed62cc601d2e8d40182d83814f
Stephen Brinkworth <xoman5144@gmail.com>
2017-02-07 10:19:45 -0700

    seperated all assessment specs into seperate files

---
+++ spec/assessmentSpecPart1.js
@@ -0,0 +1,7 @@
+describe('Part 1:', function() {
+  describe('Item1', function() {
+    it('should be a "thing"', function() {
+      expect(item1).toEqual('thing1');
+    });
+  });
+});
---
+++ spec/assessmentSpecPart3.js
@@ -0,0 +1,7 @@
+describe('Part 3:', function() {
+  describe('Item3', function() {
+    it('should be a "thing"', function() {
+      expect(item3).toEqual('thing3');
+    });
+  });
+});
