e1fe517b02ab04885c0cb01d3111d601d2e6d338
Joe Blank <joe.blank@live.com>
2017-02-09 01:23:26 -0700

    finished closure problems and tests

--- spec/assessmentSpecPart2.js
+++ spec/assessmentSpecPart2.js
@@ -1,6 +1,6 @@
 describe('Part 2:', function() {
 
-  describe('Accountant', function() {
+  describe('accountant', function() {
 
     it('accountingOffice should exist', function() {
       expect(accountingOffice).toEqual(jasmine.any(Function));
@@ -16,4 +16,64 @@ describe('Part 2:', function() {
     })
 
   })
+
+  describe('rememberall', function() {
+
+    it('forgetter should exist', function() {
+      expect(forgetter).toEqual(jasmine.any(Function));
+    })
+
+    it('forgetter should return function', function() {
+      expect(forgetter()).toEqual(jasmine.any(Function))
+    })
+
+    it('rememberall returns object', function() {
+      var rem = forgetter('Neville');
+      expect(rem('broomstick')).toEqual(jasmine.any(Object));
+    })
+
+    it('object contains name and correct array', function() {
+      var neville = forgetter('neville');
+      neville('hogwarts');
+      neville('hp');
+      neville('hogsmead');
+      neville('quidditch');
+      var resultsNeville = neville('butterbeer');
+      expect(resultsNeville.remember.join('')).toEqual('hogwartshphogsmeadquidditchbutterbeer');
+      expect(resultsNeville.name).toEqual('neville');
+      expect(resultsNeville.remember).toEqual(jasmine.any(Array));
+
+    })
+  })
+
+  describe('frodo', function() {
+
+    it('should return object with two methods', function() {
+      expect(frodo()).toEqual(jasmine.any(Object));
+      expect(frodo().dinnerOverFire).toEqual(jasmine.any(Function));
+      expect(frodo().hidingInBush).toEqual(jasmine.any(Function));
+    })
+
+    it('expect correct hunger/danger values after method invoke', function() {
+      var firstTest = frodo(50,50);
+      firstTest.dinnerOverFire();
+      firstTest.dinnerOverFire();
+      firstTest.dinnerOverFire();
+      var firstTestValues = firstTest.dinnerOverFire();
+      expect(firstTestValues.hunger).toEqual(0)
+      expect(firstTestValues.danger).toEqual(100)
+      var secondTest = frodo(0,100);
+      secondTest.hidingInBush();
+      secondTest.hidingInBush();
+      secondTest.hidingInBush();
+      secondTest.hidingInBush();
+      var secondTestValues = secondTest.hidingInBush();
+      expect(secondTestValues.hunger).toEqual(100);
+      expect(secondTestValues.danger).toEqual(0);
+    })
+  })
+
+
+
+//end of part2
 });
--- src/assessment-part2.js
+++ src/assessment-part2.js
@@ -6,8 +6,80 @@
 
 //Create a function called accountingOffice. This function will take in a parameter called assets and return a function called accountant. accountant will take in a parameter called liabilities and return assets added to liabilities.
 
+//Code here:
 function accountingOffice(assets) {
   return function accountant(liabilities) {
     return assets + liabilities;
   }
 }
+
+//Create a function called forgetter that takes in a person's name as a parameter. forgetter helps keep track of things people don't want to forget.
+//This function needs to return a function called rememberall. rememberall takes in a parameter (an item to be remembered). When rememberall is invoked, it will store the new item to be remembered along with all other previous items.
+//rememberall needs to return an object with the following format:
+// {
+//     name: << name of person, ex. Neville >>,
+//     remember: << array of items to be remembered >>
+// };
+
+//coder here:
+function forgetter(name) {
+  var itemsToRemember = []
+  return function rememberall(item) {
+    itemsToRemember.push(item);
+    return {
+      name: name,
+      remember: itemsToRemember
+    };
+  }
+}
+
+
+//Create a function called frodo. frodo will take in two parameters: startingHungerValue and startingDangerValue.
+
+//frodo will have two local variables called hunger and danger. Assign those variables values from the parameters.
+
+//frodo will return an object with two methods. The first method will be called dinnerOverFire. dinnerOverFire will decrease hunger by 25 and will increase danger by 40.
+
+//the second method will be called hidingInBush. hidingInBush will increase hunger by 35 and decrease danger by 20.
+
+//Both methods need to return an object structured like this:
+//{
+//   hunger: << hunger value >>,
+//   danger: << danger value >>
+// }
+
+//NOTE: Neither hunger nor danger should be able to exceed 100 or drop below 0.
+
+function frodo(startingHungerValue, startingDangerValue) {
+  var hunger = startingHungerValue;
+  var danger = startingDangerValue;
+  return {
+    dinnerOverFire: function() {
+        hunger = hunger - 25;
+        danger = danger + 40;
+        if(hunger < 0) hunger = 0;
+        if(danger > 100) danger = 100;
+        console.log('h',hunger,'d', danger);
+        return {
+          hunger: hunger,
+          danger: danger
+        }
+    },
+    hidingInBush: function() {
+        hunger = hunger + 35;
+        danger = danger - 20;
+        if (hunger > 100) hunger = 100;
+        if (danger < 0) danger = 0;
+        console.log('h',hunger,'d', danger);
+        return {
+          hunger: hunger,
+          danger: danger
+        }
+    }
+  }
+}
+
+var yaya = frodo(50,50);
+yaya.dinnerOverFire();
+yaya.dinnerOverFire();
+yaya.dinnerOverFire();
