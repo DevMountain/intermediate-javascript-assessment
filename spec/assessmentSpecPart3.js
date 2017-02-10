452ec663143a6f8f1ae9342449616e6649ea1441
Stephen Brinkworth <xoman5144@gmail.com>
2017-02-09 19:54:24 -0700

    first try for section 3 problems

--- spec/assessmentSpecPart3.js
+++ spec/assessmentSpecPart3.js
@@ -1,5 +1,60 @@
 describe('Part 3:', function() {
 
+  describe('callBinding', function() {
+
+    beforeEach(function() {
+      spyOn(binding, 'call');
+
+      callBinding(bindObjectArray, binding.call, 2);
+    });
+
+    it('should be a function', function() {
+			expect(callBinding)
+				.toEqual(jasmine.any(Function));
+    });
+
+    var updatedObj = callBinding(bindObjectArray, binding.call, 2);
+
+    it('should return an object', function() {
+      expect(updatedObj).toEqual(jasmine.any(Object));
+    });
+
+    it('should have updated object using call on updateAnimal function', function() {
+      expect(updatedObj.name).toEqual('Trogdor');
+    });
+
+    it('should have called the updateAnimal function with call', function() {
+      expect(binding.call).toHaveBeenCalled();
+    });
+  });
+
+  describe('applyBinding', function() {
+    beforeEach(function() {
+      spyOn(binding, 'apply');
+
+      applyBinding(bindObjectArray, binding.apply, 1);
+    });
+
+    it('should be a function', function() {
+			expect(applyBinding)
+				.toEqual(jasmine.any(Function));
+    });
+
+    var updatedObj = applyBinding(bindObjectArray, binding.apply, 1);
+    
+    it('should return an object', function() {
+      expect(updatedObj).toEqual(jasmine.any(Object));
+    });
+
+    it('should have updated object using apply on updateAnimal function', function() {
+      expect(updatedObj.favorites.join(', ') === ['Waffles', 'Butterflys', 'being majestic', 'eating rainbows'].join(', ')).toEqual(true);
+    });
+
+    it('should have called the updateAnimal function with apply', function() {
+      expect(binding.apply).toHaveBeenCalled();
+    });
+  });
+
 	describe('promiseMe', function() {
 
 		it('should be a function', function() {
@@ -14,12 +69,12 @@ describe('Part 3:', function() {
 				.toEqual(jasmine.any(Function));
 		});
 
-		it('should not change foo before resolve', function() {
+		it('should not change foo before timeout runs', function() {
 			expect(foo)
 				.toBe(undefined);
 		});
 
-		it('should change foo after resolve', function(done) {
+		it('should change foo to \'bar\' on timeout', function(done) {
 			promise
 				.then(function(response) {
 					expect(foo)
@@ -30,8 +85,28 @@ describe('Part 3:', function() {
 
 	});
 
-	describe('middleMan', function() {
-		middleMan($q, $http)
+	describe('emailList', function() {
+
+    it('should be a function', function() {
+			expect(emailList)
+				.toEqual(jasmine.any(Function));
+    });
+
+    var promise = emailList($q, $http);
+
+    it('should return a promise', function() {
+			expect(promise.then)
+				.toEqual(jasmine.any(Function));
+		});
+
+		it('should return an array of emails from the request', function(done) {
+			promise
+				.then(function(response) {
+					expect(response.join(', ') === emails.join(', '))
+						.toEqual(true);
+					done();
+				});
+		});
 
 	});
 
