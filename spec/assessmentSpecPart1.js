6517f9a5374f4149e433adaeff80f462664b1623
Matthew French <aundmr@gmail.com>
2017-02-09 23:08:22 -0700

    Working on part 1

--- spec/assessmentSpecPart1.js
+++ spec/assessmentSpecPart1.js
@@ -1,7 +1,139 @@
-describe('Part 1:', function() {
-  describe('item1', function() {
-    it('should be a thing', function() {
-      expect(item1).toEqual('thing1');
+describe('Problem 1:', function() {
+  describe('fairyTale1', function() {
+    it('should be an array', function() {
+      expect(fairyTale1).toEqual(jasmine.any(Array));
+    });
+    it('should contain the correct function names', function() {
+      var solArr1 = fairyTale1.map(function(e){return e = e.toLowerCase()})
+      var correctSolution = false
+      if(solArr1.length == 2 && solArr1.includes('mamabear') === true && solArr1.includes('papabear') === true){
+        correctSolution = true;
+      }
+      expect(correctSolution).toBe(true);
+    });
+  });
+  describe('fairyTale2', function() {
+    it('should be an array', function() {
+      expect(fairyTale2).toEqual(jasmine.any(Array));
+    });
+    it('should contain the correct function names', function() {
+      var solArr2 = fairyTale2.map(function(e){return e = e.toLowerCase()})
+      var correctSolution = false
+      if(solArr2.length == 1 && solArr2[0] == 'goldilocks'){
+        correctSolution = true;
+      }
+      expect(correctSolution).toBe(true);
+    });
+  });
+  describe('fairyTale3', function() {
+    it('should be an array', function() {
+      expect(fairyTale3).toEqual(jasmine.any(Array));
+    });
+    it('should contain the correct function names', function() {
+      var solArr3 = fairyTale3.map(function(e){return e = e.toLowerCase()})
+      var correctSolution = false
+      if(solArr3.length == 1 && solArr3[0] == 'mamabear'){
+        correctSolution = true;
+      }
+      expect(correctSolution).toBe(true);
+    });
+  });
+  describe('fairyTale4', function() {
+    it('should be an array', function() {
+      expect(fairyTale4).toEqual(jasmine.any(Array));
+    });
+    it('should contain the correct function names', function() {
+      var solArr4 = fairyTale4.map(function(e){return e = e.toLowerCase()})
+      var correctSolution = false
+      if(solArr4.length == 4 && solArr4.includes('mamabear') === true && solArr4.includes('papabear') === true && solArr4.includes('goldilocks') === true && solArr4.includes('dabears') === true){
+        correctSolution = true;
+      }
+      expect(correctSolution).toBe(true);
+    });
+  });
+  describe('fairyTale5', function() {
+    it('should be an array', function() {
+      expect(fairyTale5).toEqual(jasmine.any(Array));
+    });
+    it('should contain the correct function names', function() {
+      var solArr5 = fairyTale5.map(function(e){return e = e.toLowerCase()})
+      var correctSolution = false
+      if(solArr5.length == 4 && solArr5.includes('mamabear') === true && solArr5.includes('papabear') === true && solArr5.includes('babybear') === true && solArr5.includes('dabears') === true){
+        correctSolution = true;
+      }
+      expect(correctSolution).toBe(true);
     });
   });
 });
+
+describe('Problem 2:', function(){
+  describe('Vehicle', function(){
+    it('should be a constructor function', function(){
+      expect(new Vehicle).toEqual(jasmine.any(Object));
+    })
+    it('should have the correct property and value', function(){
+      var lambo = new Vehicle()
+      if(lambo.gasLeft === 100){
+        var propertyChecker = true;
+      }
+      expect(propertyChecker).toBe(true);
+    })
+  })
+  describe('Prototype method "drive"', function(){
+    it('should be a function', function(){
+      var prototypeMethod = Vehicle.prototype.drive
+      expect(prototypeMethod).toEqual(jasmine.any(Function));
+    })
+    it('should subtract the correct amount from gasLeft', function(){
+      var corvette = new Vehicle()
+      corvette.drive()
+      if(corvette.gasLeft === 75){
+        var methodWorkingFine = true
+      }
+      expect(methodWorkingFine).toBe(true);
+    })
+  })
+  describe('Mustang', function(){
+    it('should be an object', function(){
+      expect(mustang).toEqual(jasmine.any(Object));
+    })
+    it('should have a working drive method', function(){
+      var gasInTank = mustang.gasLeft
+      mustang.drive()
+      var couldDriveIt = false
+      if(mustang.gasLeft == gasInTank - 25){
+        couldDriveIt = true
+      }
+      expect(couldDriveIt).toBe(true)
+    })
+    it('should have the right amount of gas', function(){
+      var enoughGasLeft = false
+      if(mustang.gasLeft === 25){
+        enoughGasLeft = true;
+      }
+      expect(enoughGasLeft).toEqual(true)
+    })
+  })
+  describe('Charger', function(){
+    it('should be an object', function(){
+      expect(charger).toEqual(jasmine.any(Object));
+    })
+    it('should have a working drive method', function(){
+      var gasInTank = charger.gasLeft
+      charger.drive()
+      var couldDriveIt = false
+      if(charger.gasLeft == gasInTank - 25){
+        couldDriveIt = true
+      }
+      expect(couldDriveIt).toBe(true)
+    })
+    it('should have the right amount of gas', function(){
+      var enoughGasLeft = false
+      if(charger.gasLeft === 50){
+        enoughGasLeft = true;
+      }
+      expect(enoughGasLeft).toEqual(true)
+    })
+  })
+    
+})
--- src/assessment-part1.js
+++ src/assessment-part1.js
@@ -1,14 +1,138 @@
-// Part 1:
-// Scope,
-// Using an existing promise (were checking for use of .then and a callback)
-// Context - Implicit and new keyword
-// Type checking
-// Prototypes
+// INTERMEDIATE JAVASCRIPT ASSESSMENT
+// PART 1
 
-var item1 = 'thing1';
+// **
+// PROBLEM 1
+// **
 
-function handleIt(promise) {
-  promise .then(function(response) {
+  // Given the following nested functions:
+
+function daBears(){
+  var isFurry = true;
+  
+  function papaBear (){
+    var porridge = "Too Hot!";
+    var chair = "Too Big!";
+    var bed = "Too Hard!";
+    var feeling = "Angry";
     
-  });
+    function mamaBear(){
+      var porridge = "Too Cold!";
+      var bed = "Too Soft!";
+      
+      function babyBear(){
+        var porridge = "Just right!";
+        var chair = "Just right!";
+        var bed = "Just right!";
+        var feeling = "Whiny";
+        var sleepy = "Very yes";
+      }
+    }
+  }
+  
+  function goldilocks(){
+    var feeling = "Hungry";
+    var isFurry = false;
+    var isDinner = true;
+  }
 }
+
+//["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+
+// Remove entries from the following arrays until only correct answers remain. 
+// A correct answer will be an array of strings that are function names.
+
+// Which function(s) access the "chair" variable and get "Too Big!" 
+// (Delete wrong answers, leave correct ones)
+
+var fairyTale1 = ["papaBear", "mamaBear"];
+
+// Which function(s) access the "feeling" variable and get "Hungry" 
+// (Delete wrong answers, leave correct ones)
+
+var fairyTale2 = ["goldilocks"];
+
+// Which function(s) access the "porridge" variable and get "Too Cold!" 
+// (Delete wrong answers, leave correct ones)
+
+var fairyTale3 = ["mamaBear"];
+
+// Which functions access the "sleepy" variable and get undefined 
+// (Delete wrong answers, leave correct ones)
+
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
+
+// Which functions access the isFurry variable and get true 
+// (Delete wrong answers, leave correct ones)
+
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
+
+// -----------------------------------------------------------------------------
+
+// **
+// PROBLEM 2
+// **
+
+// Write a constructor function called Vehicle.  Vehicle should have a single property called gasLeft that is equal to 100.  
+
+// Next, assign a function called drive to the Vehicle prototype.  When invoked, drive should subtract 25 from the gasLeft property of any Vehicle your constructor function creates.
+
+// Create 2 new Vehicles with the constructor function you made: one called "charger", the other called "mustang".  Using implicit context, invoke the drive method on "charger" once, and invoke it twice on "mustang".
+
+// Code here
+
+function Vehicle(){
+  this.gasLeft = 100;
+}
+
+Vehicle.prototype.drive = function(){
+  this.gasLeft -= 25;
+}
+
+console.log(Vehicle)
+console.log(Vehicle.prototype)
+
+var mustang = new Vehicle();
+
+console.log(mustang)
+
+mustang.drive()
+
+console.log(mustang)
+
+var charger = new Vehicle();
+charger.drive()
+mustang.drive()
+
+// -----------------------------------------------------------------------------
+
+// **
+// PROBLEM 3
+// **
+// Prototypes
+// #8  ###################
+// # Prototype 1
+// Add a prototype function to the Array type that doubles the value of every item in the array and returns the modified array.
+
+// -----------------------------------------------------------------------------
+
+// **
+// PROBLEM 4
+// **
+// TYPE CHECKING
+// #12  ###################
+// # Type checking
+// Write a function called compareValues that takes in 2 parameters.
+// If both parameters are the same type and the same value return "Exact match".
+// If both parameters have the same value but are different types return "Different types".
+// Otherwise return "Different values".
+
+// Type checking
+// -----------------------------------------------------------------------------
+
+// **
+// PROBLEM 5
+// **
+// THIS CHAINING
+
+// Using an existing promise (were checking for use of .then and a callback)
