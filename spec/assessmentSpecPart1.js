bfec2dedaf593272704ffff16035502b6aeaa2c4
Matthew French <aundmr@gmail.com>
2017-02-10 02:00:23 -0700

    thigns

--- spec/assessmentSpecPart1.js
+++ spec/assessmentSpecPart1.js
@@ -193,7 +193,37 @@ describe("Problem 5:", function(){
       expect(promiseCatcher).toEqual(jasmine.any(Function))
     })
     it("should catch a promise, then return its value", function(){
+      promiseCatcher($q)
       
+      describe('promiseMe', function() {
+    
+    		it('should be a function', function() {
+    			expect(promiseMe)
+    				.toEqual(jasmine.any(Function));
+    		});
+    
+    		var promise = promiseMe($q);
+    
+    		it('should return a promise', function() {
+    			expect(promise.then)
+    				.toEqual(jasmine.any(Function));
+    		});
+    
+    		it('should not change foo before timeout runs', function() {
+    			expect(foo)
+    				.toBe(undefined);
+    		});
+    
+    		it('should change foo to \'bar\' on timeout', function(done) {
+    			promise
+    				.then(function(response) {
+    					expect(foo)
+    						.toEqual('bar');
+    					done();
+    				});
+    		});
+    
+    	});
     })
   })
 })
