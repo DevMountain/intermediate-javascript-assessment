e3ade2e78d36b63896e1f39a4da11447fc1cdabb
Stephen Brinkworth <xoman5144@gmail.com>
2017-02-20 16:19:53 -0700

    updated questions

--- src/assessment-part3.js
+++ src/assessment-part3.js
@@ -1,38 +1,58 @@
-// PROBLEM 12 (Explicit Binding)
+// INTERMEDIATE JAVASCRIPT ASSESSMENT
+// PART 3
+
+// *************
+// * PROBLEM 1 *
+// *************
+
 // For this question, you are asked to make a function called 'callBinding'.
-// This function will take in 3 parameters.
-// (magicAnimals<Array>, updateAnimal<function>, id<number>)
-// Find the animal with the matching id, then call the function
+// This function will take in 3 parameters:
+// magicAnimals (Array), updateAnimal (Function), id (Number).
+// Find the animal that matches the given id, then call the function
 // with the context of the animal, and 'Trogdor' as a parameter.
 
 // CODE HERE...
 
 
-// PROBLEM 13 (Explicit Binding)
+
+// *************
+// * PROBLEM 2 *
+// *************
+
 // For this question, you are asked to make a function called 'applyBinding'.
-// This function will take in 3 parameters.
-// (magicAnimals<Array>, updateAnimal<function>, id<number>)
-// Find the animal with the matching id, then call the function
-// with the context of the animal, and ['being majestic', 'eating rainbows'] as a parameter.
+// This function will take in 3 parameters:
+// magicAnimals (Array), updateAnimal (Function), id (Number).
+// Find the animal that matches the given id, then call the function
+// with the context of the animal, and the array ['being majestic', 'eating rainbows'] as a parameter.
 
 // CODE HERE...
 
 
-// PROBLEM 14 (Custom Promises) - :NOTE $q is an injected library that works like the angular one
+
+// *************
+// * PROBLEM 3 *
+// *************
+
+//:NOTE $q is an injected library that works like angular's $q object.
 // For this question, you are asked to make a function called 'promiseMe'.
-// This function will take in 1 parameter.
-// ($q<Custom promise object>)
-// In your function, create a custom promise, then in a time out, update the variable foo (seen below)
-// to 'bar' and complete your promise.
+// This function will take in 1 parameter:
+// $q (Custom promise object).
+// In your function, create a custom promise, then in a timeout, update the variable foo (seen below)
+// to equal 'bar' and complete your promise.
 
 var foo;
 // CODE HERE...
 
 
-// PROBLEM 15 (Custom Promises) - :NOTE $http is a function created to simulate the angular $http.
+
+// *************
+// * PROBLEM 4 *
+// *************
+
+// :NOTE $http is a function created to simulate the angular $http.
 // For this question, you are asked to make a function called 'emailList'.
-// This function will take in 2 parameters.
-// ($q<Custom promise object>, $http<Custom request function>)
+// This function will take in 2 parameters:
+// $q (Custom promise object), $http (Custom request function).
 // Set up your custom promise, Then make a request using $http.
 // GET '/api/users'
 // Make an array of emails from the returned data,
