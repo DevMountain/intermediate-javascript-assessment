beee0b15fedb861ae640cbaaff10d821005f2443
HubKaliber <[deepwell@yourlookmarketing.com]>
2018-04-18 20:30:03 -0600

    Changing Cap Letters to Lower Case

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -43,27 +43,27 @@ function daBears(){
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["papaBear", "babyBear"];
+var fairyTale1 = ["papaBear", "mamaBear"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["papaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["papaBear", "mamaBear", "babyBear"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["babyBear"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -77,12 +77,12 @@ var fairyTale5 = ["daBears", "goldilocks"];
 // drive should subtract 25 from the gasRemaining property of any Vehicle your constructor
 // function creates.
 
-var Vehicle = function() {
+function Vehicle(){
   this.gasRemaining = 100;
-
+  
 }
-Vehicle.prototype.drive = function() {
-  this.gasRemaining = this.gasRemaining - 25;
+Vehicle.prototype.drive = function(){
+    this.gasRemaining = this.gasRemaining - 25;
 }
 
 // Create 2 new Vehicles with the constructor function you made: one called "charger",
@@ -90,12 +90,12 @@ Vehicle.prototype.drive = function() {
 // "charger" once, and invoke it twice on "mustang".
 
 // CODE HERE...
-var dodge = new Vehicle();
-var corvette = new Vehicle();
+var charger = new Vehicle();
+var mustang = new Vehicle();
 
-dodge.drive();
-corvette.drive();
-corvette.drive();
+charger.drive();
+mustang.drive();
+mustang.drive();
 
 
 
@@ -122,12 +122,18 @@ corvette.drive();
 
 
 // CODE HERE...
-String.prototype.grammarPolice = function() {
-  var arr = this.split('')
-  for (var x = 0; x < arr.length; x++) {
-    arr[x] = arr[x].charAt(0).toUpperCase() + arr[x].slice(1).toLowerCase();
+String.prototype.grammarPolice = function(){
+  newString = this.toLowerCase().split(" ");
+  
+  for(var i = 0; i < newString.length; i++){
+    
+    newString[i] = newString[i].charAt(0).toUpperCase() + newString[i].substring(1);
+    
   }
-  return arr.join(' ');
+  
+  newString = newString.join(" ");
+  
+  return newString;
 }
 
 
@@ -147,14 +153,18 @@ String.prototype.grammarPolice = function() {
 
 // CODE HERE...
 // Jasmine Passing Arguments Through Parameters
-function valueType(param1, param2) {
-  if (param1 === param2) {
-    return "Exactly The Same";
-  } else if (param1 == param2) {
-    return "Same Value Different Types";
+function valueType(num1, num2){
+  if(typeof num1 === typeof num2 && num1 === num2){
+    return "Exactly the same";
   }
-  return "Different Values";
+  else if(num1 == num2 && typeof num1 !== typeof num2){
+    return "Same value, different types";
+  }
+  else{
+    return "Different values";
+  }
 }
+valueType(1, "babies");
 
 // *************
 // * PROBLEM 5 *
